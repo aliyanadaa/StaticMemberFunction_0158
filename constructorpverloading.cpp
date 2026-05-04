08ed23f333a03e086d2cfd555dcf4e63077c4c16
aliyanadaa <aliyanadaq@gmail.com>
2026-05-04 16:04:20 +0700

    menambahkan constructor default dan cosntructor overloading pada class mahasiswa

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -6,4 +6,20 @@ class mahasiswa{
     private:
         int nim;
         string nama;
+    public:
+        mahasiswa(){
+            nim = 0;
+            nama = "";
+        }
+        mahasiswa(int iNim){
+            nim = iNim;
+        }
+        mahasiswa(string iNama){
+            nama = iNama;
+        }
+
+        mahasiswa(int iNim, string iNama){
+            nim = iNim;
+            nama = iNama;
+        }
 };
